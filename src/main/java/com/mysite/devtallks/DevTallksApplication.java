9d2ad3b6404a9699cd55a702de10922bdece3daa
Dev-Hwany <qktpf7@gmail.com>
2025-12-06 03:06:25 +0900

    first commit

---
+++ src/main/java/com/mysite/devtallks/DevTallksApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.devtallks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTallksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTallksApplication.class, args);
+	}
+
+}
